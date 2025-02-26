ab0cbb3a295da2086b3a89c171fdee8f3ef39534
Shivam Lad <shivamvlad3@gmail.com>
2025-02-26 19:08:18 +0530

    Add project files.

---
+++ StudentResult/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace StudentResult
+{
+    enum Options
+    {
+        Aggregate = 1,
+        MinMark = 2,
+        MaximumMark = 3,
+        Grade = 4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            Console.Write("Enter student name : ");
+            student1.Name = Console.ReadLine();
+            Console.WriteLine("Enter student marks : ");
+            for (int i = 0; i < student1.Marks.Length; i++)
+            {
+                Console.Write($"Enter marks of subject {i + 1} : ");
+                student1.Marks[i] = Convert.ToDecimal(Console.ReadLine());
+            }
+            int choice;
+            do{
+                Console.WriteLine($"Enter choice : \n" +
+                    $"1 - Aggregate: Displays the Name: Average marks \r\n2 - MinMark: Displays the minimum marks of the student\r\n3 - " +
+                    $"MaximumMark: Displays the maximum mark\r\n4 - Grade: Displays grade\r\n0 - Exit Application ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch ((Options)choice)
+                {
+                    case Options.Aggregate:
+                        Console.WriteLine($"{student1.Name} : {student1.CalculateAverageMarks()}");
+                        break;
+                    case Options.MinMark:
+                        Console.WriteLine($"{student1.Name} : {student1.Marks.Min()}");
+                        break;
+                    case Options.MaximumMark:
+                        Console.WriteLine($"{student1.Name} : {student1.Marks.Max()}");
+                        break;
+                    case Options.Grade:
+                        Console.WriteLine($"{student1.Name} : {student1.CalculateGrade(student1.CalculateAverageMarks())}");
+                        break;
+                    default:
+                        if(choice!=0)
+                        Console.WriteLine("Enter a valid choice !!!!");
+                        break;
+                }  
+            }while (choice != 0);
+        }
+    }
+}
---
+++ StudentResult/Student.cs
@@ -0,0 +1,37 @@
+﻿namespace StudentResult
+{
+    class Student
+    {
+        public string Name;
+        public decimal[] Marks = new decimal[5];
+        static decimal AverageMarks;
+        public decimal CalculateAverageMarks()
+        {
+            AverageMarks = Marks.Average();
+            return AverageMarks;
+        }
+        public string CalculateGrade(decimal marks)
+        {
+            string grade;
+            switch (marks)
+            {
+                case > 90:
+                    grade = "A";
+                    break;
+                case > 80:
+                    grade = "B";
+                    break;
+                case > 70:
+                    grade = "C";
+                    break;
+                case < 70:
+                    grade = "D";
+                    break;
+                default:
+                    grade = "";
+                    break;
+            }
+            return grade;
+        }
+    }
+}
